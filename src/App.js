8049030d50dfb16bf2cf0f9c84fe6ec3dd18f69e
Richard1m <officialrichardm1@gmail.com>
2021-12-03 17:29:49 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -6,20 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <hi> Hello World </hi>
+        <p> Hosting a website is very easy with Github pages </p>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
