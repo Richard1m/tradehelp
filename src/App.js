9f6bd8e4d0046ae2f9144d8e2b1a9077e77b6147
Richard1m <officialrichardm1@gmail.com>
2021-12-03 18:45:57 -0800

    'fot'

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <hi> Hello World </hi>
+        <p> Hosting a website is very easy with Github pages </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
