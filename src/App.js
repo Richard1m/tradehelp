0f4f3fe0ece3848a73f9081602d2e97681997391
Richard1m <officialrichardm1@gmail.com>
2021-12-04 07:25:57 -0800

    last

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <hi> Hello World </hi>
         <p> Hosting a website is very easy with Github pages </p>
       </header>
